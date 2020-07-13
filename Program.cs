195a7c71d2a25b22680b23e49872af58460a42a6
Denys Matveiev <gradash@gmail.com>
2020-07-13 12:26:23 +0300

    111

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using MySql.Data.MySqlClient;
 
 namespace learning
@@ -15,59 +14,33 @@ namespace learning
 
              static void Main(string[] args)
         {
-
-            // database connect
-
-            string connStr = "server=35.233.55.137;user=root;database=learning;password=qwerty_1;";
-            MySqlConnection conn = new MySqlConnection(connStr);
-            conn.Open();
-
-
-            // YES/NO loop for adding text
+            dbConnect dbConnect = new dbConnect();
             string restart = null;
             do
             {
                 Console.WriteLine("Enter text to database: ");
                 string addstring = Console.ReadLine();
-                
+                dbConnect.InsertDB(addstring);
 
-                // insert to database without injection
-                string query = "INSERT INTO learning (content) VALUES (@content);";
-                MySqlCommand dbInsert = new MySqlCommand(query, conn);
-                dbInsert.Parameters.AddWithValue("@content", addstring);
-                dbInsert.ExecuteNonQuery();
-
-
-
-                // YES/NO loop for adding text
-                Console.Write("Add more? (Y/N) ");                          
+                Console.Write("Add more? (Y/N) ");
                 restart = Console.ReadLine().ToUpper();
-                while ((restart != "Y") && (restart != "N"))   
+                while ((restart != "Y") && (restart != "N"))
                 {
                     Console.WriteLine("Error");
                     Console.WriteLine("Add more? (Y/N) ");
                     restart = Console.ReadLine().ToUpper();
                 }
-            } while (restart == "Y");                                        
+            } while (restart == "Y");
 
 
-            //printing what in database
-
-                Console.WriteLine("------------------");
-                Console.WriteLine("Now in Database: ");
-                string sqlRead = "SELECT id,content FROM learning";
-                MySqlCommand dbRead = new MySqlCommand(sqlRead, conn);
-                MySqlDataReader reader = dbRead.ExecuteReader();
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString());
-                }
-                reader.Close();
-                
-                
-
-            conn.Close();
+            var result = dbConnect.ReadDB();
+            
+            Console.WriteLine("------------------");
+            Console.WriteLine("Now in Database: ");
+            Console.WriteLine(result);
             Console.ReadKey();
+
+                                          
         }
     }
 }
---
+++ dbConnect.cs
@@ -0,0 +1,63 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning
+{
+    class dbConnect
+    {
+        private readonly string connStr = "server=35.233.55.137;user=root;database=learning;password=qwerty_1;";
+
+
+        public void InsertDB(string addstring)
+        {
+
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+
+            string query = "INSERT INTO learning (content) VALUES (@content);";
+            MySqlCommand dbInsert = new MySqlCommand(query, conn);
+            dbInsert.Parameters.AddWithValue("@content", addstring);
+            dbInsert.ExecuteNonQuery();
+
+            conn.Close();
+
+        }
+
+        public string ReadDB()
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            conn.Open();
+
+
+            string sqlRead = "SELECT id,content FROM learning";
+            MySqlCommand dbRead = new MySqlCommand(sqlRead, conn);
+            MySqlDataReader reader = dbRead.ExecuteReader();
+
+            string result = string.Empty;
+            while (reader.Read())
+            {
+                result = result + (reader[0].ToString() + " " + reader[1].ToString()) + "\n";
+            }
+            reader.Close();
+
+            conn.Close();
+            return result;
+        }
+
+
+    }
+
+
+}
+
+
+
+
+    
+
